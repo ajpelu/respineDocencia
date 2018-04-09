c63a93ced83fc3f640a34707ada35822d845152b
Antonio <ajpelu@gmail.com>
2018-04-09 14:07:00 +0200

    get all pkgs script

---
+++ pkgs_requirements.R
@@ -0,0 +1,21 @@
+# library("here")
+# library("checkpoint")
+# library("grateful") 
+# 
+# 
+# all_pkgs <- scan_packages(all.pkgs = TRUE, include.Rmd = TRUE)
+# all_pkgs 
+
+install.packages(c("RColorBrewer", 
+                   "dplyr",
+                   "knitr",
+                   "landscapeR",
+                   "markdown",
+                   "raster",          
+                   "rasterVis", 
+                   "rgeos",      
+                   "shiny",         
+                   "shinycssloaders",
+                   "shinydashboard",
+                   "shinythemes",
+                   "sp"))
